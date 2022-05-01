33ea54d325f3c9b8db40333f5f1b7c55948adc77
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-05-02 03:07:16 +0430

    Update get_weth.py

--- Aave lend and borrow/scripts/get_weth.py
+++ Aave lend and borrow/scripts/get_weth.py
@@ -5,6 +5,6 @@ def get_weth():
     
     account = accounts.load('mywaalet')
     weth = interface.IWeth("0xd0A1E359811322d97991E03f863a0C30C2cF029C")
-    tx = weth.deposit({"from": account, "value": 0.001 * 10 ** 18 })
+    tx = weth.deposit({"from": account, "value": 0.01 * 10 ** 18 })
     tx.wait(2)
     return tx
